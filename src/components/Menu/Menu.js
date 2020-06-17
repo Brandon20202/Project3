4ad3f30ce7a0db3430f6785cf4b92f3150ff69e1
Felipe Cantu <36748934+ABACABB00@users.noreply.github.com>
2020-06-16 21:21:14 -0500

    forgot to make the side panel active with authentication

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,19 +1,31 @@
 import React from 'react';
+import { useAuth0 } from "../../react-auth0-spa";
 
 import './Menu.css'
 
-const menuDrawer = props => {
+const menu = props => {
     let drawerClasses = 'menu_drawer';
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
     if (props.show) {
         drawerClasses = 'menu_drawer open';
     }
     return (<nav className={drawerClasses}>
         <ul>
             <li><a href="/">Services</a></li>
-            <li><a href="/">Login</a></li>
+            <div>
+                {!isAuthenticated && (
+                    <button onClick={() => loginWithRedirect({})}>Login</button>
+                )}
+
+                {isAuthenticated && <button onClick={() => logout()}>Log Out</button>}
+            </div>
         </ul>
     </nav>
     )
 }
 
-export default menuDrawer;
+
+
+
+export default menu;
