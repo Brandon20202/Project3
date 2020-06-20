74ed175545a2ef89a697b43f633b805b2b796abe
Brandon20202 <brandon.taylor@austinisd.org>
2020-06-20 11:33:57 -0500

    Changed server.js file to represent new DB

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
-var express = require('express');
+const express = require("express");
 
-var app = express();
-var PORT = process.env.PORT || 8080;
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-var db = require('./models');
-
-app.use(express.urlencoded({extended: true}));
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Add routes, both API and view
+app.use(routes);
 
-app.use(express.static('public'));
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/project3");
 
-require('./routes/companies-api-routes.js')(app);
-
-db.sequelize.sync({force: true}).then(function () {
-	app.listen(PORT, function () {
-		console.log('App listening on PORT' + PORT);
-	});
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
+
